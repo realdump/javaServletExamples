4bd87bdda80cff1d3b6b9e39d33a8d2284acb3d6
realdump <a.majeed17@outlook.com>
2024-04-26 09:33:42 +0530

    update

---
+++ app4/src/main/java/com/abmn/WelcomeServlet.java
@@ -0,0 +1,35 @@
+package com.abmn;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class WelcomeServlet
+ */
+@WebServlet("/Welcome")
+public class WelcomeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("<font size='6' color='red'>");
+		out.println("<b>");
+		out.println("Welcome");
+		out.println("<hr>");
+		out.println("</b></font></body></html>");
+		
+	}
+
+}
